10e1cf9f64e17dbd8fa8cc54f2f483ff7f3adaa8
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-08-19 14:59:40 +0100

    logger for iyan/main/rs

--- iyan/iyan/main.rs
+++ iyan/iyan/main.rs
@@ -7,6 +7,8 @@ use std::io::Read;
 use actix::prelude::*; 
 use std::{env, fs::File};
 
+use logger::{init_elasticsearch_logger};
+
 use core::db::postgres;
 fn main() {
     env::set_var(
@@ -15,8 +17,15 @@ fn main() {
     );
     env_logger::init();
 
+    init_elasticsearch_logger("http://logstash:5000");
+
+    log::info!("This is an info log message.");
+    log::error!("This is an error log message.");
+
     // empty strings
     let mut settings = String::new();
+
+    log::info!("reading iyan.toml");
 
     File::open(
         format!("{}/iyan.toml", env!("HOME")).as_str(),
@@ -29,7 +38,8 @@ fn main() {
     let postgres_url = config.postgres.clone();
     let pg_pool = postgres::init_pool(&postgres_url);
     let postgres = SyncArbiter::start(4, move || postgres::PgExecutor(pg_pool.clone()));
-
+    log::info!("Running server");
     server::run(postgres, config);
+    log::info!("Server up and running");
     let _ = system.run();
 }
--- iyan/logger/src/lib.rs
+++ iyan/logger/src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use log::{Record, Level, Metadata};
+use reqwest::Client;
+use serde_json::json;
+
+pub struct ElasticsearchLogger {
+    client: Client,
+    logstash_url: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl ElasticsearchLogger {
+    pub fn new(logstash_url: &str) -> Self {
+        ElasticsearchLogger {
+            client: Client::new(),
+            logstash_url: logstash_url.to_string(),
+        }
     }
+}
+
+impl log::Log for ElasticsearchLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Info // You can adjust the log level here
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            let log_entry = json!({
+                "timestamp": chrono::Utc::now(),
+                "level": format!("{:?}", record.level()),
+                "message": record.args().to_string(),
+            });
+
+            _ = self.client
+                .post(&self.logstash_url)
+                .header("Content-Type", "application/json") 
+                .body(log_entry.to_string()) 
+                .send();
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+pub fn init_elasticsearch_logger(logstash_url: &str) -> Result<(), log::SetLoggerError> {
+    log::set_max_level(log::LevelFilter::Info); 
+    log::set_boxed_logger(Box::new(ElasticsearchLogger::new(logstash_url)))
 }
