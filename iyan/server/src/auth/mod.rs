0d1f471492de732cf24636a54fabc6620ac9db1c
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-08-21 07:22:12 +0100

    iyan services mod

--- iyan/server/src/auth/mod.rs
+++ iyan/server/src/auth/mod.rs
@@ -1,10 +1,11 @@
-use actix_web::{error, Error as ActixError, FromRequest, HttpMessage, HttpRequest, dev::Payload};
+use actix_web::{error, Error as ActixError, FromRequest, web, HttpRequest, dev::Payload};
 use serde::{Serialize, Deserialize};
 use chrono::prelude::*;
-use futures::future::{err, Future, ready, Ready};
-use jsonwebtoken;
+use futures::future::{ready, Ready};
+use jsonwebtoken::{encode, EncodingKey, DecodingKey};
 use uuid::Uuid;
-
+use futures::future;
+use super::state::AppState;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct JWTPayload {
@@ -14,7 +15,6 @@ pub struct JWTPayload {
 }
 
 impl JWTPayload {
-
     pub fn new(user: Option<AuthUser>, client: Option<AuthClient>, exp: DateTime<Utc>) -> Self {
         JWTPayload {
             client,
@@ -25,8 +25,8 @@ impl JWTPayload {
 
     pub fn encode(&self, jwt_private: &Vec<u8>) -> Result<String, jsonwebtoken::errors::Error> {
         let header = jsonwebtoken::Header::new(jsonwebtoken::Algorithm::RS256);
-
-        jsonwebtoken::encode(&header, &self, jwt_private)
+        let jwt_private = EncodingKey::from_rsa_pem(jwt_private).expect("Failed to create EncodingKey");
+        encode(&header, &self, &jwt_private)
     }
 }
 
@@ -36,27 +36,27 @@ impl FromRequest for JWTPayload {
     type Future = Ready<Result<Self, Self::Error>>; 
 
     fn from_request(req: &HttpRequest, _payload: &mut Payload) -> Self::Future {
-        let state = req.state();
+        let state = req.app_data::<web::Data<AppState>>().unwrap();
 
         let auth_header = match req.headers().get("authorization") {
             Some(auth_header) => auth_header,
-            None => return Err(error::ErrorUnauthorized("invalid authorization token")),
+            None => return future::err(error::ErrorUnauthorized("invalid authorization token")),
         };
 
         let auth_header_parts: Vec<_> = auth_header.to_str().unwrap().split_whitespace().collect();
         if auth_header_parts.len() != 2 {
-            return Err(error::ErrorUnauthorized("invalid authorization token"));
+            return future::err(error::ErrorUnauthorized("invalid authorization token"));
         }
 
         if auth_header_parts.len() != 2 || auth_header_parts[0].to_lowercase() != "bearer" {
-            return Err(error::ErrorUnauthorized("invalid authorization token"));
+            return future::err(error::ErrorUnauthorized("invalid authorization token"));
         }
 
         let validation = jsonwebtoken::Validation::new(jsonwebtoken::Algorithm::RS256);
-
-        match jsonwebtoken::decode::<JWTPayload>(&auth_header_parts[1], &state.jwt_public, &validation) {
-            Ok(token) => Ok(token.claims),
-            Err(_) => Err(error::ErrorUnauthorized("invalid authorization token")),
+        let jwt_public = DecodingKey::from_rsa_pem(&state.jwt_public).expect("Failed to create DecodingKey");
+        match jsonwebtoken::decode::<JWTPayload>(&auth_header_parts[1], &jwt_public, &validation) {
+            Ok(token) => ready(Ok(token.claims)),
+            Err(_) => future::err(error::ErrorUnauthorized("invalid authorization token")),
         }
     }
 }
