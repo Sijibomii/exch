6a12c05cf61ec71eb41c85df1f92bac95bf8b088
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-08-21 09:52:34 +0100

    logstash conf

--- egusi/lib/egusi.ex
+++ egusi/lib/egusi.ex
@@ -46,4 +46,22 @@ defmodule Egusi do
        ]}
     ]
   end
+
+  # each user can only be in one or more trading rooms. trading rooms hold the user ids of memebers
+  # it calls their user sessions to send them some ws update
+  defp start_trading_sessions() do
+
+  end
+
+  # TODO: bake this into the supervision tree itself by having the
+  # supervisor fetch and look up the the list of online voice servers
+  # by querying GCP tags.
+  defp start_rabbits() do
+    IO.puts("about to start_rabbits")
+
+    # start the rabbit supervised
+    # start the online rabbit supervised
+
+    IO.puts("finished rabbits")
+  end
 end
