ef593eb8a04598c71805c87e41d4993846173d05
Ajayi Sijibomi <sijibomiolajubu@gmail.com>
2023-09-02 16:14:07 +0100

    ticker session

--- egusi/TODO.md
+++ egusi/TODO.md
@@ -6,3 +6,12 @@ One websocket connection, elixir app spins up a usersession for that user using 
 Each ticker has a ticker session. that keeps track of the current state of the order book and can give to a user on request
 
 websocket messages are ansered by the user session. If it's a message making a trade, the session for that ticker is called
+
+
+possible rabbit msgs
+
+new ticker
+incemental
+snapshot
+new order
+new login
--- egusi/lib/Onion/ticker_session.ex
+++ egusi/lib/Onion/ticker_session.ex
@@ -5,7 +5,6 @@ defmodule Onion.TickerSession do
   defmodule Order do
     @type t :: %{
             id: String.t(),
-            user_id: String.t(),
             side: String.t(),
             operation: String.t(),
             time: String.t(),
@@ -13,7 +12,6 @@ defmodule Onion.TickerSession do
           }
 
     defstruct id: nil,
-              user_id: nil,
               side: nil,
               operation: nil,
               time: nil,
@@ -31,6 +29,53 @@ defmodule Onion.TickerSession do
               trading_id: "",
               order_book: []
   end
+
+
+  #################################################################################
+  # REGISTRY AND SUPERVISION BOILERPLATE
+
+  defp via(ticker_id), do: {:via, Registry, {Onion.TickerSessionRegistry, ticker_id}}
+
+  defp cast(ticker_id, params), do: GenServer.cast(via(ticker_id), params)
+  defp call(ticker_id, params), do: GenServer.call(via(ticker_id), params)
+
+  def start_supervised(initial_values) do
+    callers = [self() | Process.get(:"$callers", [])]
+
+    case DynamicSupervisor.start_child(
+           Onion.TickerSessionDynamicSupervisor,
+           {__MODULE__, Keyword.merge(initial_values, callers: callers)}
+         ) do
+      {:error, {:already_started, pid}} -> {:ignored, pid}
+      error -> error
+    end
+  end
+
+  def child_spec(init), do: %{super(init) | id: Keyword.get(init, :ticker_id)}
+
+  def count, do: Registry.count(Onion.TickerSessionRegistry)
+  def lookup(user_id), do: Registry.lookup(Onion.TickerSessionRegistry, user_id)
+
+
+  ###############################################################################
+  ## INITIALIZATION BOILERPLATE
+  def start_link(init) do
+    GenServer.start_link(__MODULE__, init, name: via(init[:ticker_id]))
+  end
+
+  def init(init) do
+    # adopt callers from the call point.
+    Process.put(:"$callers", init[:callers])
+    {:ok, struct(State, init)}
+  end
+
+   ########################################################################
+  ## API
+
+
+  ########################################################################
+  ## ROUTER
+
 
 
 end
